0f60299a0c0efcf8c8addb1a9d54a36135669344
ipeng5 <ypeng115@gmail.com>
2022-08-26 23:21:34 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// appendNum(num) - when click a number, it adds to the current screen
+
+// update current screen (down)
+
+// clear
+
+// delete
+
+// choose operation. chooseOperation(operation){}
+
+// computer - take values inside the calculate and compute a single value that needss to display on the calculator
+
+//update display to update value inside of our output
